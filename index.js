920b1a750aabec3a8083a0421ab909d9e3b50c90
Alex <alexurban12@gmail.com>
2021-11-26 22:22:49 +0200

    change message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // require('dotenv').config();
-const airdropMessage = "Multiply your KAI with 1 click - kardiainfo.com/game" + "\n\n"
+const airdropMessage = "Farm INFO on kardiainfo.com/farm" + "\n\n"
 
 const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
